dc3e65b62122c1efbc09c163e94e79c570ccb0d0
hieule <leconghieu1998@gmail.com>
2020-01-14 23:20:10 +0700

    refactor code

--- shape.h
+++ shape.h
@@ -1,38 +1,38 @@
 #ifndef __SHAPE_H_INCLUDED__
 #define __SHAPE_H_INCLUDED__
 
-#include "vec3.h"
+#include "vec4.h"
 
 class Shape{
 	private:
 		char character;
-		unsigned char** shapes;
+		unsigned char **states;
 		int width;
 		int height;
-		Vec3 color;
+		Vec4 color;
 		int num_state;
 		int current_state;
-
-		Shape operator=(const Shape &&);
-		unsigned char* gen_shape(unsigned char* spawn_shape);
+		
+		Shape(const Shape &);
+		void free();
 		void print(int state);
 	public:
 		Shape();
-		Shape(char ch, unsigned char* spawn_shape, int w, int h, Vec3 col, int num_state);
-		Shape(const Shape &) = default;
-		Shape& operator=(const Shape &) = default;
+		Shape(char ch, unsigned char **states, int w, int h, Vec4 col, int num_state);
+		Shape& operator=(const Shape &);
+		Shape& operator=(const Shape &&);
 		~Shape();
-		char get_character();
-		int get_width();
-		int get_height();
-		Vec3 get_color();
-		unsigned char* get_shape();
-		int get_num_state();
+		char get_character() const;
+		int get_width() const;
+		int get_height() const;
+		Vec4 get_color() const;
+		unsigned char* get_shape() const;
+		int get_num_state() const;
 		void change_state(int n);
-		int get_state(int n);
-		int get_current_state();
+		unsigned char **get_states() const;
+		int get_current_state() const;
 		void print();
-		static Shape* load_shape(char* filename);
+		static Shape* load_shape(const char* filename);
 };
 
 #endif
