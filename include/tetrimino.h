2d79cb9e65b687bf46c163a62477d15612161d06
hieule <leconghieu1998@gmail.com>
2020-01-23 22:16:34 +0700

    rm color attri and change array type

--- include/tetrimino.h
+++ include/tetrimino.h
@@ -6,10 +6,9 @@
 class Tetrimino{
 	private:
 		char character;
-		unsigned char **states;
+		Vec4 **states;
 		int width;
 		int height;
-		Vec4 color;
 		int num_state;
 		int current_state;
 		
@@ -18,21 +17,20 @@ class Tetrimino{
 		void print(int state);
 	public:
 		Tetrimino();
-		Tetrimino(char ch, unsigned char **states, int w, int h, Vec4 col, int num_state);
+		Tetrimino(char ch, Vec4 **states, int w, int h, int num_state);
 		Tetrimino& operator=(const Tetrimino &);
 		Tetrimino& operator=(const Tetrimino &&);
 		~Tetrimino();
 		char get_character() const;
 		int get_width() const;
 		int get_height() const;
-		Vec4 get_color() const;
-		unsigned char* get_tetrimino() const;
+		Vec4 *get_tetrimino() const;
 		int get_num_state() const;
 		void change_state(int n);
-		unsigned char **get_states() const;
+		Vec4 **get_states() const;
 		int get_current_state() const;
 		void print();
-		static Tetrimino* load_tetrimino(const char* filename);
+		static Tetrimino *load_tetrimino(const char *filename);
 };
 
 #endif
--- source/tetrimino.cpp
+++ source/tetrimino.cpp
@@ -4,12 +4,11 @@
 
 Tetrimino::Tetrimino(){}
 
-Tetrimino::Tetrimino(char ch, unsigned char **states, int w, int h, Vec4 col, int num_state){
+Tetrimino::Tetrimino(char ch, Vec4 **states, int w, int h, int num_state){
 	this->character = ch;
 	this->states = states;
 	this->width = w;
 	this->height = h;
-	this->color = col;
 	this->num_state = num_state;
 	this->current_state = 0;
 }
@@ -21,7 +20,6 @@ Tetrimino& Tetrimino::operator=(const Tetrimino &tetrimino){
 	this->states = tetrimino.get_states();
 	this->width = tetrimino.get_width();
 	this->height = tetrimino.get_height();
-	this->color = tetrimino.get_color();
 	this->num_state = tetrimino.get_num_state();
 	this->current_state = tetrimino.get_current_state();
 	return *this;
@@ -34,7 +32,6 @@ Tetrimino& Tetrimino::operator=(const Tetrimino &&tetrimino){
 	this->states = tetrimino.get_states();
 	this->width = tetrimino.get_width();
 	this->height = tetrimino.get_height();
-	this->color = tetrimino.get_color();
 	this->num_state = tetrimino.get_num_state();
 	this->current_state = tetrimino.get_current_state();
 	return *this;
@@ -69,11 +66,7 @@ int Tetrimino::get_height() const{
 	return this->height;
 }
 
-Vec4 Tetrimino::get_color() const{
-	return this->color;
-}
-
-unsigned char *Tetrimino::get_tetrimino() const{
+Vec4 *Tetrimino::get_tetrimino() const{
 	return this->states[current_state];
 }
 
@@ -88,7 +81,7 @@ void Tetrimino::change_state(int n){
 		this->current_state += this->num_state;
 }
 
-unsigned char **Tetrimino::get_states() const{
+Vec4 **Tetrimino::get_states() const{
 	return this->states;
 }
 
@@ -99,10 +92,15 @@ int Tetrimino::get_current_state() const{
 void Tetrimino::print(int state_num){
 	int i = 0;
 	int j = 0;
+	Vec4 vec;
 	while(i < this->height){
 		j = 0;
 		while(j < this->width){
-			std::cout<<this->states[state_num][i*this->width+j];
+			if(this->states[state_num][i*this->width+j] == vec){
+				std::cout<<'0';
+			}
+			else
+				std::cout<<'1';
 			j++;
 		}
 		std::cout<<std::endl;
@@ -121,7 +119,7 @@ void Tetrimino::print(){
 Tetrimino *Tetrimino::load_tetrimino(const char *filename){
 	Tetrimino *tetriminos;
 	char ch;
-	unsigned char **states;
+	Vec4 **states;
 	int w, h, r, g, b, a;
 	w = h = r = g = b = a = 0;
 	Vec4 col;
@@ -132,27 +130,31 @@ Tetrimino *Tetrimino::load_tetrimino(const char *filename){
 	infile >> n;
 	
 	tetriminos = new Tetrimino[n];
-	int i = 0, j = 0, k = 0, m = 0;
+	int i = 0, j = 0, k = 0, m = 0, is_not_zezo;
 	while(i < n){
 		infile >> ch >> num_state >> h >> w;
+		infile >> r >> g >> b;
+		col.set(r, g, b, 1);
+
 		j = 0;
-		states = new unsigned char*[num_state];
+		states = new Vec4*[num_state];
 		while(j < num_state){
 			k = 0;
-			states[j] = new unsigned char[w*h];
+			states[j] = new Vec4[w*h];
 			while(k < h){
 				m = 0;
 				while(m < w){
-					infile>>states[j][k*w+m];
+					infile>>is_not_zezo;
+
+					if(is_not_zezo)
+						states[j][k*w+m] = col;
 					m++;
 				}
 				++k;
 			}
 			++j;
 		}
-		infile >> r >> g >> b;
-		col.set(r, g, b, 1);
-		Tetrimino tetrimino(ch, states, w, h, col, num_state);
+		Tetrimino tetrimino(ch, states, w, h, num_state);
 		tetriminos[i] = tetrimino;
 		tetrimino.free();
 		++i;
